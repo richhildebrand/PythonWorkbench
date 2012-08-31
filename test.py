32daa5b594da3d4d2e36aef3a319a5aa9aa95a1d
Rich <Rich@rich.com>
2012-08-31 18:40:50 -0400

    pprint import

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
+import pprint
+
 print "Hello world"
 
-def test():
-	print "fine indent"
+a = "mike"
+b = 5
+c = "corey"
 
+def test():
+	rich = "richard"
+	print "fine indent"
+	pprint.pprint(locals())
 test()
+
+print "\nsecond scope\n"
+
+pprint.pprint(locals())
