0a0756fd4d72174c7c03d3eda06cba8b999328a3
Rich <Rich@rich.com>
2012-08-31 20:50:07 -0400

    eval and exec examples

--- test.py
+++ test.py
@@ -1,17 +1,31 @@
 #https://github.com/pythoncapstone/capstone.git
 import pprint
+def wrapperFunction():
 
-a = "mike"
-b = 5
-c = "corey"
+	a = "mike"
+	b = 5
+	c = "corey"
 
-def testFunction():
-	rich = "richard"
-	print "\nttestFunction scope\n"
+	def testFunction():
+		rich = "richard"
+		print "\ntestFunction scope\n"
+		pprint.pprint(locals())
+
+	testFunction()
+
+	seth = 'a = "tom"'
+	seth2 = 'a + "ben"'
+	#empty set limits globals
+	exec(seth) in {}, locals()
+	result = eval(seth2, {}, locals())
+	
+	print seth
+	print result
+
+	print "\nwrapper scope\n"
 	pprint.pprint(locals())
 
-testFunction()
+wrapperFunction()
 
 print "\nglobal scope\n"
-
 pprint.pprint(locals())
