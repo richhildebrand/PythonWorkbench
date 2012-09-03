216b9797dd1e09e11d241df719d9f5b456b77dc9
Rich <Rich@rich.com>
2012-09-02 20:20:04 -0400

    moved getPdcInstructions to FileBuilder

--- UserCode/PythonFileBuilder.py
+++ UserCode/PythonFileBuilder.py
@@ -1,20 +1,25 @@
 class PythonFileBuilder:
-		LINES_ADDED = 2
+	LINES_ADDED_WHEN_BUILDING_FILE = 5
 
-		def __init__(self, code):	
-			self.code = code
+	def __init__(self, code):	
+		self.code = code
 
-		def buildFile(self):
-			self.__addTabToNewLines()
-			self.__addWrapperFunction()
-			self.__addCallForWrapperFunction()
-			return self.code
+	def buildFile(self):
+		self.__addTabToNewLines()
+		self.__addWrapperFunction()
+		self.__addCallForWrapperFunction()
+		return self.code
 
-		def __addTabToNewLines(self):
-			self.code = self.code.replace('\n', '\n\t')
+	def __addTabToNewLines(self):
+		self.code = self.code.replace('\n', '\n\t')
 
-		def __addWrapperFunction(self):
-			self.code = 'def WrapperFunction():\n\t' + self.code
+	def __addWrapperFunction(self):
+		self.code = 'def WrapperFunction():\n\t' + self.code
 
-		def __addCallForWrapperFunction(self):
-			self.code = self.code + "\nWrapperFunction()"
+	def __addCallForWrapperFunction(self):
+		self.code = self.code + "\nWrapperFunction()"
+	
+	def getPdcInstructions(self, stepNumber):
+		instructions = "step;;" * (stepNumber + self.LINES_ADDED_WHEN_BUILDING_FILE)
+		instructions = instructions + "locals()"
+		return instructions
--- UserCode/UserCodeManager.py
+++ UserCode/UserCodeManager.py
@@ -5,18 +5,20 @@ from PythonLib import PythonLib
 class UserCodeManager:
 	USER_FILE_PATH = "UserFiles/"
 
-	def __init__(self, userID, userCode, stepNumber):
+	def __init__(self, userID, userCode):
 		self.userID = userID
 		self.userCode = userCode
-		self.stepNumber = stepNumber + PythonFileBuilder.PythonFileBuilder.LINES_ADDED
-	
+		self.stepNumber = 0
+		self.pythonFileBuilder = PythonFileBuilder.PythonFileBuilder(self.userCode)
+		self.userCode = self.pythonFileBuilder.buildFile()
+
 	def executeUserCode(self):
+		self.stepNumber = self.stepNumber + 1
 		self.__createUserCodeFile()
 		self.__runFile()
 
 	def __createUserCodeFile(self):
-		pythonFileBuilder = PythonFileBuilder.PythonFileBuilder(self.userCode)
-		self.userCode = pythonFileBuilder.buildFile()
+
 		try:
 			userCodeFile = open(self.userID+'CodeFile.py', 'w+')
 			userCodeFile.write(self.userCode)
@@ -26,7 +28,7 @@ class UserCodeManager:
 	def __runFile(self):
 		defaultStdin = sys.stdin
 		defaultStdout = sys.stdout
-		sys.stdin = io.StringIO(self.__getPdcInstructions())
+		sys.stdin = io.StringIO(self.pythonFileBuilder.getPdcInstructions(self.stepNumber))
 		PythonLib.ensureDirectory(self.USER_FILE_PATH)
 		sys.stdout = open(self.USER_FILE_PATH + self.userID + 'ResultFile.txt', 'w+')
 		try:
@@ -38,7 +40,3 @@ class UserCodeManager:
 			sys.stdout.close()
 			sys.stdin = defaultStdin
 			sys.stdout = defaultStdout
-
-	def __getPdcInstructions(self):
-			instructions = "step;;" * self.stepNumber
-			return instructions
