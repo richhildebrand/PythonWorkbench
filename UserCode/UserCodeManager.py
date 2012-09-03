a5b870295a51f561dcc47d2bec19489c24be550e
Rich <Rich@rich.com>
2012-09-03 00:16:41 -0400

    moved IO logic to customIOManager

--- UserCode/UserCodeManager.py
+++ UserCode/UserCodeManager.py
@@ -1,36 +1,37 @@
 import sys, io, pdb
 import PythonFileBuilder
+import CustomIOManager
 from PythonLib import PythonLib
 
 class UserCodeManager:
 	USER_FILE_PATH = "UserFiles/"
+	pythonFileBuilder = None
+	userCodeFilePath = None
+	stepNumber = None
+	userCode = None
+	userID = None
 
 	def __init__(self, userID, userCode):
-		self.pythonFileBuilder = PythonFileBuilder.PythonFileBuilder()
 		self.stepNumber = 0
-		self.userCode = userCode
 		self.userID = userID
-		self.__onInit()
-
+		self.userCode = userCode
+		self.pythonFileBuilder = PythonFileBuilder.PythonFileBuilder()
+		self.userCodeFilePath =	self.pythonFileBuilder.buildFile(self.userCode, self.userID)
+		
 	def executeUserCode(self):
 		self.stepNumber = self.stepNumber + 1
 		self.__runFile()
 
-	def __onInit(self):
-		self.userCodeFilePath =	self.pythonFileBuilder.buildFile(self.userCode, self.userID)
-
 	def __runFile(self):
-		defaultStdin = sys.stdin
-		defaultStdout = sys.stdout
-		sys.stdin = io.StringIO(self.pythonFileBuilder.getPdcInstructions(self.stepNumber))
-		PythonLib.ensureDirectory(self.USER_FILE_PATH)
-		sys.stdout = open(self.USER_FILE_PATH + self.userID + 'ResultFile.txt', 'w+')
+		PythonLib.ensureDirectoryExists(self.USER_FILE_PATH)
+		outputFromPDB = open(self.USER_FILE_PATH + self.userID + 'ResultFile.txt', 'w+')
+		inputForPDB = io.StringIO(self.pythonFileBuilder.getPdcInstructions(self.stepNumber))
+
+		customIOManager = CustomIOManager.CustomIOManager()
+		customIOManager.setCustomIO(inputForPDB, outputFromPDB)
 		try:
 			#thread.start_new_thread(pdb.run, ('import ' + self.userID + 'CodeFile', {}, locals()))
 			#Thread(target=pdb.run, args=('import ' + self.userID + 'CodeFile', {}, locals())).start()
 			pdb.run('import ' + self.userCodeFilePath, {}, {})
 		finally:
-			sys.stdin.close()
-			sys.stdout.close()
-			sys.stdin = defaultStdin
-			sys.stdout = defaultStdout
+			customIOManager.cleanUpCustomIO()
