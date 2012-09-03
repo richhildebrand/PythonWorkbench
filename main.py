cf8ffe2ee36923fb1c041625e8e4e4e2b6870b7e
Rich <Rich@rich.com>
2012-09-03 17:06:47 -0400

    adjust print statements in main

--- main.py
+++ main.py
@@ -2,24 +2,21 @@ import sys
 from UserCode import UserCodeManager
 
 userOne = 'noMethods'
-
-simpleCode = 'print "line 1"\nprint "line2"\n'
-simpleCode = simpleCode + 'a=3\nb=4\nc = a + b\n'
-simpleCode = simpleCode + 'print a\nprint b\nprint c'
+simpleCode = 'a=3\nb=4\nc = a + b\nd = a + b * c'
 
 userCodeManager = UserCodeManager.UserCodeManager(userOne, simpleCode)
-userCodeManager.executeUserCode()
+print 'Simple Code Example'
 userCodeManager.executeUserCode()
 userCodeManager.executeUserCode()
 userCodeManager.executeUserCode()
 userCodeManager.executeUserCode()
 
 userTwo = 'hasMethods'
-
 methodCode = 'tim="Tim"\ndef hello(name):\n\treturn "Hello " + name\n'
 methodCode = methodCode + 'tim = hello(tim)\nseth = "Seth"\nseth= hello(seth)'
 
 userCodeManager = UserCodeManager.UserCodeManager(userTwo, methodCode)
+print "MethodCode Example"
 userCodeManager.executeUserCode()
 userCodeManager.executeUserCode()
 userCodeManager.executeUserCode()
