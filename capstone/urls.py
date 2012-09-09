70382aa89a863aaa7edadc4c102b03a199f6119a
Rich <Rich@rich.com>
2012-09-09 11:38:55 -0400

    adjusted url path for student

--- capstone/urls.py
+++ capstone/urls.py
@@ -8,13 +8,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'capstone.views.home', name='home'),
     # url(r'^capstone/', include('capstone.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
-    # Custom URLs
-    url(r'^$', 'public.views.index')
+    url(r'^student/$', 'student.views.index')
 )
