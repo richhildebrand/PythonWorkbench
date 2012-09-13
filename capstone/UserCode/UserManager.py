d40510c409fc11feb94fc829ac40174bf7e71815
Rich <Rich@rich.com>
2012-09-13 10:31:59 -0400

    added example code for users

--- capstone/UserCode/UserManager.py
+++ capstone/UserCode/UserManager.py
@@ -6,12 +6,8 @@ class UserManager:
 	def __init__(self):
 		self.userCodeManagers = {}
 
-	# For multiple users
-	# Will check for a current user object, and delete it before making a new one
 	def createUserCodeManager(self, user, code):
 			self.userCodeManagers[user] = UserCodeManager.UserCodeManager(user, code)
 
-	# For multiple users
-	# Will find the correct userCodeManage to step into
 	def executeStepInUserCode(self, user):
 		return self.userCodeManagers[user].executeStepInUserCode()
