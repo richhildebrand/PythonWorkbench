db38089c3a80e2c203d70435e28b8c3dac912bca
Micah Detamore <mdetamor@kent.edu>
2012-09-21 15:35:16 -0400

    File parser additions

--- capstone/UserCode/UserCodeManager.py
+++ capstone/UserCode/UserCodeManager.py
@@ -1,5 +1,6 @@
 import io, pdb
 import PythonFileBuilder
+import FileParser
 from capstone.PythonLib import PythonLib
 from collections import namedtuple
 
@@ -10,6 +11,7 @@ class UserCodeManager:
 	stepNumber = None
 	userCode = None
 	userID = None
+	userCodeException = None
 
 	def __init__(self, userID, userCode):
 		self.stepNumber = 0
@@ -29,19 +31,22 @@ class UserCodeManager:
 		inputForDebugger = io.StringIO(unicode(self.pythonFileBuilder.getPdcInstructions(self.stepNumber)))
 
 		debugger = pdb.Pdb(completekey='tab', stdin=inputForDebugger, stdout=outputFromDebugger)
+		self.userCodeException = ''
+		
 		try:
 			debugger.run('import ' + self.userCodeFilePath, {}, {})
 		except Exception, e:
-			print e
+			self.userCodeException = e
 		finally:
 			outputFromDebugger.close()
 			inputForDebugger.close()
 
 	def __resultOfStepInUserCode(self):
 		# Need replace with actual exception; localVars; lineNumber
+		fileParser = FileParser.FileParser(self.USER_FILE_PATH + self.userID + 'ResultFile.txt')
 		userStepResult = {}
 		userStepResult['exception'] = open(self.USER_FILE_PATH + self.userID + 'ResultFile.txt', 'r').read()
-		userStepResult['localVars'] = "localVars"
-		userStepResult['lineNumber'] = 42
+		userStepResult['localVars'] = fileParser.get_local_vars()
+		userStepResult['lineNumber'] = fileParser.get_current_line()
 
 		return userStepResult
