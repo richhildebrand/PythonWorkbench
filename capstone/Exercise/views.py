4d4e564fb8bdd99d459e1df4a5c1f7fd30e4b63b
Rich <Rich@rich.com>
2012-09-21 22:56:43 -0400

    fixed syntax errors in view

--- capstone/Exercise/views.py
+++ capstone/Exercise/views.py
@@ -1,11 +1,15 @@
-from django.template import Context, loader
-from Exercise.models import Exercise
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import Context, RequestContext, loader
 
 def displayExercises(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
-    t = loader.get_template('exerciseView.html')
-    c = Context({
-        'exerciseList': exerciseList,
-    })
-    return HttpResponse(t.render(c))
+	print "inside displayExercises"
+	try:
+		from capstone.Exercise.models import Exercise
+		latest_poll_list = Exercise.objects.all().order_by('-pub_date')[:5]
+		t = loader.get_template('exerciseView.html')
+		c = Context({'exerciseList': exerciseList })
+		return HttpResponse(t.render(c))
+	except Exception, e:
+		return HttpResponse(e)
+	return HttpResponse("hi")
