2916c10db30527f7721083ea8febefa04f85704b
Rich <Rich@rich.com>
2012-09-21 21:51:26 -0400

    exercises will now display their name in admin

--- capstone/Exercise/models.py
+++ capstone/Exercise/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 class Exercise(models.Model):
+	def __unicode__(self):
+        return self.name
+
     name = models.CharField(max_length=50)
     wordProblem = models.TextField()
-    methodBody = models.TextField()
+    methodBody = models.CharField(max_length=250)
 
 class MethodCall(models.Model):
     exercise = models.ForeignKey(Exercise)
-    methodCall = models.TextField()
+    methodCall = models.CharField(max_length=250)
     expectedResult = models.CharField(max_length=100)
