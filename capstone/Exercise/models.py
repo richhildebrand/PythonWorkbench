43689527bee875c555b84b02e6cf7dfc61119f48
Rich <Rich@rich.com>
2012-09-21 22:09:38 -0400

    fixed indentation error

--- capstone/Exercise/models.py
+++ capstone/Exercise/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 class Exercise(models.Model):
 	name = models.CharField(max_length=50)
-    wordProblem = models.TextField()
-    methodBody = models.CharField(max_length=250)
+	wordProblem = models.TextField()
+	methodBody = models.CharField(max_length=250)
 
 	def __unicode__(self):
-        return self.name
+		return self.name
 
 class MethodCall(models.Model):
     exercise = models.ForeignKey(Exercise)
