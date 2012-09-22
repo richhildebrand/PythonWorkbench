960e6ab528cf3c77d83ca0ec4a21af949dea5780
Rich <Rich@rich.com>
2012-09-21 22:06:27 -0400

    reordered method in Exercise

--- capstone/Exercise/models.py
+++ capstone/Exercise/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
 class Exercise(models.Model):
-	def __unicode__(self):
-        return self.name
-
-    name = models.CharField(max_length=50)
+	name = models.CharField(max_length=50)
     wordProblem = models.TextField()
     methodBody = models.CharField(max_length=250)
+
+	def __unicode__(self):
+        return self.name
 
 class MethodCall(models.Model):
     exercise = models.ForeignKey(Exercise)
